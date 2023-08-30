c39ac15b8ef646ee345ea85720c74a89b49e45f0
Сергей <mak.sergei@ngs.ru>
2023-08-31 00:10:24 +0700

    4

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -23,15 +23,7 @@ public class Faculty {
 
     }
 
-//    public Faculty(Long id, String name, String color) {
-//        this.id = id;
-//        this.name = name;
-//        this.color = color;
-//    }
-//
-//    public Faculty() {
-//
-//    }
+
 
 
     public Long getId() {
